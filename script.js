440dc10cfa3b9748931a355b4d1dc089375f51df
Joe Chamberlain <joe.chamberlain@silverstripe.com>
2025-09-24 15:27:52 +1200

    Redo all the css and finish styling for mobile

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const button = document.querySelector('button');
-const shareMenu = document.getElementById('share-menu');
+const button = document.querySelector('.share-button');
+const shareMenu = document.querySelector('.share-menu');
 const buttonIcon = document.querySelector('.share-button-image');
-
 
 // Define image sources for easier maintenance
 const defaultIcon = 'images/icon-share.svg';
@@ -9,7 +8,9 @@ const activeIcon = 'images/icon-active.svg';
 
 button.addEventListener('click', () => {
   // Toggle a single, descriptive state class
-  const isActive = shareMenu.classList.toggle('active');
+  const isActive = shareMenu.classList.toggle('share-menu-active');
+
+  shareMenu.classList.toggle('share-menu');
 
   // Use the component's state to determine the icon
   buttonIcon.src = isActive ? activeIcon : defaultIcon;
